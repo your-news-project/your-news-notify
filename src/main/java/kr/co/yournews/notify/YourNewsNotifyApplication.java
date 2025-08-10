b82cf2f8e74c7f450236cf98462365aa35b78b2f
DaeYoung0726 <pdy000726@naver.com>
2025-08-10 18:11:09 +0900

    chore: project setting

---
+++ src/main/java/kr/co/yournews/notify/YourNewsNotifyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.yournews.notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourNewsNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourNewsNotifyApplication.class, args);
+    }
+
+}
