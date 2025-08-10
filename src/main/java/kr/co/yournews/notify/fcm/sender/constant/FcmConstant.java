d0c59640c1eacb335d7477c445cece03e0aa2e2a
DaeYoung0726 <pdy000726@naver.com>
2025-08-10 22:49:24 +0900

    feat: FCM 상수 추가

---
+++ src/main/java/kr/co/yournews/notify/fcm/sender/constant/FcmConstant.java
@@ -0,0 +1,7 @@
+package kr.co.yournews.notify.fcm.sender.constant;
+
+public final class FcmConstant {
+    private FcmConstant() { }
+
+    public static final String NEWS_NOTIFICATION_CONTENT = "새로운 소식을 확인해보세요!";
+}
